cc18110737c77d1e0de069866e685287df907f82
liutaot <473139299@qq.com>
2020-03-19 13:01:25 +0800

    [Add] add date tool class

---
+++ src/dateTool.php
@@ -0,0 +1,47 @@
+<?php
+namespace dateTool;
+
+class dateTool{
+    /**
+     * 获取指定日期段内每一天的日期
+     * @param  Date  $startdate 开始日期
+     * @param  Date  $enddate   结束日期
+     * @return Array
+     */
+    public static function getDateFromRange($startdate, $enddate){
+
+        $stimestamp = strtotime($startdate);
+        $etimestamp = strtotime($enddate);
+
+        // 计算日期段内有多少天
+        $days = ($etimestamp-$stimestamp)/86400+1;
+
+        // 保存每天日期
+        $date = array();
+
+        for($i=0; $i<$days; $i++){
+            $date[] = date('Y-m-d', $stimestamp+(86400*$i));
+        }
+
+        return $date;
+    }
+
+    /**
+     * 计算机两个日期间隔的天数
+     * @param $day1
+     * @param $day2
+     * @return float|int
+     */
+    public static function diffBetweenTwoDays ($day1, $day2)
+    {
+        $second1 = strtotime($day1);
+        $second2 = strtotime($day2);
+
+        if ($second1 < $second2) {
+            $tmp = $second2;
+            $second2 = $second1;
+            $second1 = $tmp;
+        }
+        return ($second1 - $second2) / 86400;
+    }
+}
